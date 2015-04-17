1a8559989f0e82c6c63447fd48547ff6439905aa
mstamos <mstamos@gmail.com>
2015-04-17 11:15:30 +0300

    Return lessons data from database collection

--- client/templates/lessons/lessonsList.js
+++ client/templates/lessons/lessonsList.js
@@ -1,23 +1,5 @@
-var lessonsData= [
-  {
-    title: 'What is Meteor and how does it do it\'s magic?',
-    date: '21/4/2015',
-    time: '19:30',
-    location: 'SciFy'
-  }, 
-  {
-    title: 'Installation and basic concepts. ',
-    date: '5/5/2015',
-    time: '19:30',
-    location: 'SciFy'
-  }, 
-  {
-    title: 'Installation and basic concepts.',
-    date: '19/5/2015',
-    time: '19:30',
-    location: 'SciFy'
-  }
-];
 Template.lessonsList.helpers({
-  lessons: lessonsData
+  lessons: function () {
+    return Lessons.find();
+  }
 });
