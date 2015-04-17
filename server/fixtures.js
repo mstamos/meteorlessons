5f3448560ad11c4536ca2bd18b7af95eb1760d9d
mstamos <mstamos@gmail.com>
2015-04-17 10:48:33 +0300

    Added data to lessons collection.

---
+++ server/fixtures.js
@@ -0,0 +1,22 @@
+if (Lessons.find().count() === 0) {
+  Lessons.insert({
+    title: 'What is Meteor and how does it do it\'s magic?',
+    date: '21/4/2015',
+    time: '19:30',
+    location: 'SciFy'
+  });
+  
+  Lessons.insert({
+    title: 'Installation and basic concepts. ',
+    date: '5/5/2015',
+    time: '19:30',
+    location: 'SciFy'
+  });
+  
+  Lessons.insert({
+    title: 'Installation and basic concepts.',
+    date: '19/5/2015',
+    time: '19:30',
+    location: 'SciFy'
+  });
+}
