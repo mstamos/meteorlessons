3629ae30b8c73557772a4f58473e443f38e5a0c8
mstamos <themeteorpoint@gmail.com>
2015-04-22 12:49:37 +0300

    Denormalized the number of comments into the post.

--- client/templates/lessons/lessonItem.js
+++ client/templates/lessons/lessonItem.js
@@ -1,8 +1,5 @@
 Template.lessonItem.helpers({
 	ownLesson: function () {
 		return this.userId === Meteor.userId();
-	},
-	commentsCount: function() {
-		return Comments.find({lessonId: this._id}).count();
 	}
 });
--- server/publications.js
+++ server/publications.js
@@ -2,7 +2,7 @@ Meteor.publish("lessons", function () {
   return Lessons.find();
 });
 
-Meteor.publish('comments', function() {
-	check(postId, String);
-	return Comments.find({postId: postId});
+Meteor.publish('comments', function(lessonId) {
+	check(lessonId, String);
+	return Comments.find({lessonId: lessonId});
 });
