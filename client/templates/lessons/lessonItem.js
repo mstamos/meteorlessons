632dd68e63e6080619d7d23d56b6301005a88e37
mstamos <themeteorpoint@gmail.com>
2015-04-22 12:14:31 +0300

    Display comments on 'lessonPage'

--- client/templates/lessons/lessonItem.js
+++ client/templates/lessons/lessonItem.js
@@ -1,5 +1,8 @@
 Template.lessonItem.helpers({
-  ownLesson: function () {
-    return this.userId === Meteor.userId();
-  }
+	ownLesson: function () {
+		return this.userId === Meteor.userId();
+	},
+	commentsCount: function() {
+		return Comments.find({lessonId: this._id}).count();
+	}
 });
