aa1e1e2a69e5ae8462ca8c173e02ceab4f959125
mstamos <themeteorpoint@gmail.com>
2015-04-22 09:56:02 +0300

    Validate lesson contents on submission

--- client/templates/lessons/lesson_submit.js
+++ client/templates/lessons/lesson_submit.js
@@ -1,3 +1,19 @@
+Template.lessonSubmit.onCreated(function() {
+  Session.set('lessonSubmitErrors', {});
+
+});
+Template.lessonSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('lessonSubmitErrors')[field];
+
+  },
+  errorClass: function (field) {
+    return !!Session.get('lessonSubmitErrors')[field] ? 'has-error' : '';
+
+  }
+
+});
+
 Template.lessonSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -8,6 +24,11 @@ Template.lessonSubmit.events({
       location: $(e.target).find('[name=location]').val(),
       description: $(e.target).find('[name=description]').val()
     };
+
+    var errors = validatePost(lesson);
+    if (errors.title || errors.time || errors.date || errors.description || errors.presentationUrl || errors.location)
+      return Session.set('lessonSubmitErrors', errors);
+
    Meteor.call('lessonInsert', lesson, function(error, result) {
      // display the error to the user and abort
       if (error)
